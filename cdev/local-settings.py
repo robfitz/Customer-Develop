a3d4902038b6940665ecf70f9f04243ff410ea7b
Rob Fitzpatrick <robftz@gmail.com>
2010-03-31 22:06:45 -0400

    Fixed local template path. Removed Django 1.2 dependency in templates/worksheets/Features.html

--- cdev/local-settings.py
+++ cdev/local-settings.py
@@ -68,7 +68,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/www/customerdev/templates",
+    "C:/www/Customer-Develop/templates",
 )
 
 INSTALLED_APPS = (
